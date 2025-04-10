d4639207587eed61db1f63b56f3e7ffc22417860
Yichi ZHANG <freemanz771746166@163.com>
2025-04-11 00:57:31 +0800

    Trying pip install

--- makefile
+++ makefile
@@ -2,21 +2,21 @@
 # Change these for your computer configuration. #
 #################################################
 
-export MAKE = make
-export CXX = g++
-export AR = ar
-export OBJ = __CPP__
+export MAKE = __MAKE__
+export CXX = __CXX__
+export AR = __AR__
+export OBJ = __OBJ__
 # $(OBJ) should be either "__CPP__" or "__PYTHON__".
 # The two versions can coexist.
 # You may compile twice, once with "__CPP__" and the other with "__PYTHON__", to obtain both versions.
 # In the "lib/" directory, "libmaniverse.a" and "libmaniverse.so" are for CPP and "Maniverse.so" is for Python.
 
 # If $(OBJ) is "__PYTHON__", you need to set $(PYTHON3) and $(PYBIND11).
-export PYTHON3 = /home/yzhangnn/scratch/anaconda3/include/python3.11
+export PYTHON3 = __PYTHON3__
 # The path where you can find "Python.h".
-export PYBIND11 = /home/yzhangnn/pybind11/include/
-# The path where you can find "pybind11/".
-export EIGEN3 = /home/yzhangnn/eigen/
+export PYBIND11 = __PYBIND11__
+# The path where you can find "pybind11/attr.h", "pybind11/stl/", etc.
+export EIGEN3 = __EIGEN3__
 # The path where you can find "Eigen/", "signature_of_eigen3_matrix_library" and "unsupported/".
 
 
---
+++ setup.py
@@ -0,0 +1,72 @@
+import os
+import urllib.request
+import tarfile
+import subprocess
+from setuptools import setup
+from setuptools.command.build import build
+
+class CustomBuild(build):
+	def run(self):
+
+		pwd = os.path.dirname(__file__)
+
+		# Checking commands
+		MAKE = os.getenv("MAKE", default = "make")
+		print("MAKE is %s." % MAKE)
+		CXX = os.getenv("CXX", default = "g++")
+		print("CXX is %s." % CXX)
+		AR = os.getenv("AR", default = "ar")
+		print("AR is %s." % AR)
+
+		# Checking dependencies
+		PYTHON3 = os.getenv("PYTHON3")
+		print("Looking for Python.h at %s ..." % PYTHON3, end='')
+		if os.path.isfile(PYTHON3 + "Python.h"):
+			print("Found!")
+		else:
+			raise RuntimeError("Python.h does not exist!")
+		EIGEN3 = os.getenv("EIGEN3")
+		if len(EIGEN3) > 0:
+			print("Looking for Eigen3 at %s ..." % EIGEN3, end='')
+			if os.path.exists(EIGEN3 + "/Eigen/") and os.path.exists(EIGEN3 + "/unsupported/") and os.path.isfile(EIGEN3 + "/signature_of_eigen3_matrix_library"):
+				print("Found!")
+			else:
+				raise RuntimeError("Python.h does not exist!")
+		else:
+			print("The environment variable $EIGEN3 is not set. -> Downloading ...")
+			urllib.request.urlretrieve("https://gitlab.com/libeigen/eigen/-/archive/3.4-rc1/eigen-3.4-rc1.tar.gz", pwd)
+			with tarfile.open(tarball_path) as tar:
+				tar.extractall(path = pwd) # Directory: eigen-3.4-rc1
+			EIGEN3 = pwd + "/eigen-3.4-rc1/"
+		PYBIND11 = os.popen("pip show pybind11 | grep 'Location:'").split()[1] + "/pybind11/include/"
+
+		# Configuring the makefile
+		subprocess.check_call(["sed", "-i", "'%s/__MAKE__/" + MAKE + "/g", "makefile"])
+		subprocess.check_call(["sed", "-i", "'%s/__CXX__/" + CXX + "/g", "makefile"])
+		subprocess.check_call(["sed", "-i", "'%s/__AR__/" + AR + "/g", "makefile"])
+		subprocess.check_call(["sed", "-i", "'%s/__PYTHON3__/" + PYTHON3 + "/g", "makefile"])
+		subprocess.check_call(["sed", "-i", "'%s/__EIGEN3__/" + EIGEN3 + "/g", "makefile"])
+		subprocess.check_call(["sed", "-i", "'%s/__PYBIND11__/" + PYBIND11 + "/g", "makefile"])
+
+		# Make
+		nproc = os.popen("nproc")
+		subprocess.check_call(["sed", "-i", "'%s/__OBJ__/__CPP__/g", "makefile"])
+		subprocess.check_call([MAKE, "-j", nproc])
+		subprocess.check_call(["sed", "-i", "'%s/__CPP__/__PYTHON__/g", "makefile"])
+		subprocess.check_call([MAKE, "-j", nproc])
+
+		super().run()
+
+
+setup(
+		name = "Maniverse",
+		author = "FreemanTheMaverick",
+		description = "Function optimization on manifolds",
+		version = "0.2",
+		url = "https://github.com/FreemanTheMaverick/Maniverse.git",
+		packages = ["Maniverse"],
+		package_data = { "Maniverse": ["lib/*"] },
+		cmdclass = { "build": CustomBuild },
+		install_requires = ["pybind11>=2.13.6"],
+		classifiers = ["Programming Language :: Python :: 3"]
+)
