cbbeec04cd5c9cad5509ea2514bc479f78da5458
Yichi ZHANG <freemanz771746166@163.com>
2025-05-28 18:46:18 +0800

    Bug fix
    Previously pip-install would downloads all the headers, which are unnecessary. Now the headers will not be pip-installed.

--- setup.py
+++ setup.py
@@ -3,16 +3,16 @@ import urllib.request
 import tarfile
 from glob import glob
 from setuptools import setup, find_packages
-from setuptools.command.build_py import build_py
+from setuptools.command.build import build
 import pybind11
 from pybind11.setup_helpers import Pybind11Extension, ParallelCompile, naive_recompile
 
-__version__ = "0.3.3"
+__version__ = "0.3.4"
 
 # Downloading Eigen3
 pwd = os.path.dirname(__file__)
 EIGEN3 = pwd + "/eigen-3.4-rc1/"
-class CustomBuild(build_py):
+class CustomBuild(build):
 	def run(self):
 		url = "https://gitlab.com/libeigen/eigen/-/archive/3.4-rc1/eigen-3.4-rc1.tar.gz"
 		dest = pwd + "/eigen-3.4-rc1.tar.gz"
@@ -29,11 +29,12 @@ ParallelCompile(
 ).install()
 
 MV_CPP = sorted(glob("src/*.cpp") + glob("src/*/*.cpp"))
+MV_HEADER = sorted(glob("src/*.h") + glob("src/*/*.h"))
 ext_modules = [ Pybind11Extension(
 	"Maniverse",
 	MV_CPP,
-	undef_macros = ["DEBUG"],
 	include_dirs = [EIGEN3],
+	depends = MV_HEADER,
 	extra_compile_args = ["-O3", "-D__PYTHON__", "-DEIGEN_INITIALIZE_MATRICES_BY_ZERO"],
 	cxx_std = 17,
 	language = "c++"
@@ -47,13 +48,7 @@ setup(
 		long_description = open("README.md").read(),
 		long_description_content_type = "text/markdown",
 		url = "https://github.com/FreemanTheMaverick/Maniverse.git",
-		cmdclass = {"build_py": CustomBuild},
+		cmdclass = {"build": CustomBuild},
 		ext_modules = ext_modules,
-		packages = ["src", "src/Manifold", "src/Optimizer"],
-		package_data = {
-			"src": ["*.h"],
-			"src/Manifold": ["*.h"],
-			"src/Optimizer": ["*.h"],
-		},
 		classifiers = ["Programming Language :: Python :: 3"]
 )
