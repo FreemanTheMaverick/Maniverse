5af29e0b1d44b9e62cf1f76d5dd957be4a3bb5fc
FreemanTheMaverick <freemanz771746166@163.com>
2026-04-16 03:44:15 +0800

    Minor improvement

--- src/Optimizer/AugmentedLagrangian.h
+++ src/Optimizer/AugmentedLagrangian.h
@@ -24,18 +24,20 @@ namespace Maniverse{
 #ifdef __PYTHON__
 pybind11::function AugmentedLagrangian(
 		double init_rho, double theta_rho, double theta_sigma,
-		std::vector<double> tol, int max_iter, int output){ return pybind11::cpp_function([init_rho, theta_rho, theta_sigma, tol, max_iter, output](pybind11::function func) -> pybind11::cpp_function{ return pybind11::cpp_function([init_rho, theta_rho, theta_sigma, tol, max_iter, output, func](pybind11::args args, pybind11::kwargs kwargs) -> bool{
+		std::vector<double> tol, int max_iter, int output){ return pybind11::cpp_function([=](pybind11::function func) -> pybind11::cpp_function{ return pybind11::cpp_function([=](pybind11::args args, pybind11::kwargs kwargs) -> bool{
 #else
 auto AugmentedLagrangian(
 		double init_rho, double theta_rho, double theta_sigma,
-		std::vector<double> tol, int max_iter, int output){ return [init_rho, theta_rho, theta_sigma, tol, max_iter, output](auto&& func){ return [init_rho, theta_rho, theta_sigma, tol, max_iter, output, func = std::forward<decltype(func)>(func)](auto&&... args) -> bool{
+		std::vector<double> tol, int max_iter, int output){ return [=](auto&& func){ return [=, func = std::forward<decltype(func)>(func)](auto&&... args) -> bool{
 #endif
 	#ifdef __PYTHON__
 	Iterate& M = args[0].cast<Iterate&>();
 	#else
 	Iterate& M = std::get<0>(std::forward_as_tuple(args...));
 	#endif
-	const int ncons = (int)M.Func->Lambda.size();
+	std::vector<double>& Lambda = M.Func->Lambda;
+	std::vector<double>& Violation = M.Func->Constraint_Value;
+	const int ncons = Lambda.size();
 	if ( output ){
 		std::printf("***************************** Augmented Lagrangian *****************************\n\n");
 		std::printf("Number of constraints: %d\n", ncons);
@@ -45,15 +47,29 @@ auto AugmentedLagrangian(
 		std::printf("\n");
 	}
 
-	double& Rho = M.Func->Rho;
+	double& Rho = M.Func->Rho = 0;
+	std::memset(Lambda.data(), 0, ncons * 8);
 	double last_max_vio = 0;
+
+	if ( output ) std::printf("First run for the initial multipliers ...\n");
+	M.Func->Calculate(M.getPoint(), {0, 1});
+	M.setGradient();
+	const std::vector<Eigen::MatrixXd> Ge = M.Func->Gradient;
+	const Eigen::VectorXd Gf = M.Gradient;
+	Eigen::MatrixXd Gg = Eigen::MatrixXd::Zero(Gf.size(), ncons);
+	for ( int i = 0; i < ncons; i++ ){
+		M.Func->Gradient = M.Func->Constraint_Gradient[i];
+		M.setGradient();
+		Gg.col(i) = M.Gradient;
+	}
+	M.Func->Gradient = Ge;
+	M.Gradient = Gf;
+	const Eigen::VectorXd tmp = - Gg.colPivHouseholderQr().solve(Gf);
+
+	std::memcpy(Lambda.data(), tmp.data(), ncons * 8);
+	Rho = init_rho;
+
 	for ( int iiter = 0; iiter < max_iter; iiter++ ){
-		std::vector<double>& Lambda = M.Func->Lambda;
-		std::vector<double>& Violation = M.Func->Constraint_Value;
-		if ( iiter == 0 ){
-			std::memset(Lambda.data(), 0, ncons * 8);
-			Rho = 0;
-		}
 		if ( output ){
 			std::printf("\nIteration %d\n", iiter);
 			std::printf("Lagrange multipliers:");
@@ -62,49 +78,34 @@ auto AugmentedLagrangian(
 			std::printf("Penalty factor: %f\n", Rho);
 			std::printf("Running internal optimization ...\n");
 		}
-		if ( iiter == 0 ){
-			M.Func->Calculate(M.getPoint(), {0, 1});
-			M.setGradient();
-			const EigenVector Gf = M.Gradient;
-			EigenMatrix Gg = Eigen::MatrixXd::Zero(Gf.size(), ncons);
-			for ( int i = 0; i < ncons; i++ ){
-				M.Func->Gradient = M.Func->Constraint_Gradient[i];
-				M.setGradient();
-				Gg.col(i) = M.Gradient;
-			}
-			const Eigen::VectorXd tmp = - Gg.colPivHouseholderQr().solve(Gf);
-			std::memcpy(Lambda.data(), tmp.data(), ncons * 8);
-			Rho = init_rho;
-		}else{
-			#ifdef __PYTHON__
-			const bool inner_converged = pybind11::bool_(func(*args, **kwargs));
-			#else
-			const bool inner_converged = func(std::forward<decltype(args)>(args)...);
-			#endif
-			if ( ! inner_converged ) throw std::runtime_error("Internal optimization did not converge!");
-		}
+		#ifdef __PYTHON__
+		const bool inner_converged = pybind11::bool_(func(*args, **kwargs));
+		#else
+		const bool inner_converged = func(std::forward<decltype(args)>(args)...);
+		#endif
+		if ( ! inner_converged ) throw std::runtime_error("Internal optimization did not converge!");
 
 		if ( output ){
 			std::printf("Constraint violation:");
 			for ( int i = 0; i < ncons; i++ ) std::printf(" %E", Violation[i]);
 			std::printf("\n");
 		}
-		if ( iiter == 0 ) goto NotConverged;
 		for ( int i = 0 ; i < ncons; i++ ) if ( std::abs(Violation[i]) > tol[i] ) goto NotConverged;
-		if ( output ) std::printf("Converged!\n");
+		if ( output ){
+			std::printf("Converged!\n");
+			std::printf("Final Lagrange multipliers:");
+			for ( int i = 0 ; i < ncons; i++ ) std::printf(" %f", Lambda[i]);
+			std::printf("\n");
+		}
 		return true;
 
 		NotConverged:
 		if ( output ) std::printf("Not converged yet!\n");
 		const double max_vio = *std::max_element(Violation.begin(), Violation.end(), [](const int& a, const int& b){ return abs(a) < abs(b); });
-		if ( iiter > 0 ){
-			for ( int i = 0; i < ncons; i++ ){
-				Lambda[i] += Rho * Violation[i];
-			}
+		for ( int i = 0; i < ncons; i++ ){
+			Lambda[i] += Rho * Violation[i];
 		}
-		if ( iiter > 1 ){
-			if ( max_vio > theta_sigma * last_max_vio ) Rho *= theta_rho;
-		}
+		if ( iiter > 0 && max_vio > theta_sigma * last_max_vio ) Rho *= theta_rho;
 		last_max_vio = max_vio;
 	}
 	return false;
